9df3b7e098005cee720796586b092f204abf5dd5
Filipe Santos <classicmagic404@gmail.com>
2022-07-15 10:16:16 -0300

    times em ruby

---
+++ basic/aula03/times.rb
@@ -0,0 +1,6 @@
+print "Informe quantas vezes quer que eu repita essa mensagem amigo -_- : "
+how_times = gets.chomp.to_i
+
+how_times.times do
+    puts "Informe quantas vezes quer que eu repita essa mensagem amigo -_- : "
+end
