5ccc5a218e57fc76ffe7f33cf24132b0c9800002
Filipe Santos <classicmagic404@gmail.com>
2022-07-15 10:05:24 -0300

    while no ruby

---
+++ basic/aula03/while.rb
@@ -0,0 +1,7 @@
+books = ["O desafio de ferro", "A luva de bronze", "A chave de cobre", "A mascára de prata"]
+count = 1
+
+while count < books.length
+    puts "book##{count} - #{books[count]}"
+    count = count+1
+end
